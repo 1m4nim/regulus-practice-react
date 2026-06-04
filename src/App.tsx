014f6ff46558cbcf9bfd57b2ac1d67f8d57680d2
1m4nim <fancustard9@gmail.com>
2026-06-04 13:43:35 +0900

    Question08

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css"
-import Question07 from "./components/Question07";
+import Question08 from "./components/Question08";
+//import Question07 from "./components/Question07";
 //import Question06 from "./components/Question06"
 
 function App() {
   return (
     <>
       {/**<Question06 />*/}
-      <Question07 />
+      {/**<Question07 />*/}
+      <Question08 />
     </>
   )
 }
---
+++ src/components/Question08.tsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+function Question08() {
+    return (
+        <div>
+            <Button size="small" color="red" text="Small Button" />
+            <Button />
+            <Button size="large" text="クリック" />
+        </div>
+    )
+}
+
+export default Question08;
